475223fcc468fc34ed44c2447dfd55a9a6a7e6ee
Abiral <lamsal.abiral@gmail.com>
2025-12-09 15:21:29 +0545

    minor fixes

--- sql-scripts/create-user.sql
+++ sql-scripts/create-user.sql
@@ -1,5 +1,5 @@
 DROP USER if exists 'universitydba'@'%' ;
 
-CREATE USER 'universitydba'@'%' IDENTIFIED BY 'universitydba';
+CREATE USER 'universitydba'@'%' IDENTIFIED BY 'UniDba@2025!';
 
 GRANT ALL PRIVILEGES ON * . * TO 'universitydba'@'%';
